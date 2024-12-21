e6b748d4762a39d32d1f0d03d6bd5a45df97b6b6
dkrzyzak <damntheguitars@gmail.com>
2024-12-21 23:10:07 +0100

    start using env variables

--- app/db/connection.server.ts
+++ app/db/connection.server.ts
@@ -1,7 +1,8 @@
-import { Pool } from 'pg';
+import pg from 'pg';
+const { Pool } = pg;
 
 export const db = new Pool({
     max: 5,
-    connectionString: '',
+    connectionString: process.env.POSTGRES_URL,
     connectionTimeoutMillis: 2000,
 });
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -11,6 +11,8 @@ import { createReadableStreamFromReadable } from "@react-router/node";
 import { ServerRouter } from "react-router";
 import { isbot } from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
+// initialize db
+import "~/db/connection.server";
 
 const ABORT_DELAY = 5_000;
 
